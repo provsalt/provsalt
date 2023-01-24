c0be4b9501e5d4eac466ac579f74f4cf1aa8c238
Raymond <raymond.goo124@gmail.com>
2023-01-25 01:27:23 +0800

    src/lib/components/webring.svelte: Hopefully fix webring.

--- src/lib/components/webring.svelte
+++ src/lib/components/webring.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
 	import "./webring.sass"
+	let myIndex: number;
 </script>
 
 
 <div id="webring-wrapper">
-
 	{#await fetch("https://webring.hackclub.com/members.json").then(r => r.json())}
+		<a class="webring-anchor" href="https://webring.hackclub.com/" title="Previous">‹</a>
+		<a class="webring-logo" href="https://webring.hackclub.com/"
+			 title="Hack Club Webring"></a>
+		<a class="webring-anchor" href="https://webring.hackclub.com/" title="Next">›</a>
 	{:then webring}
 		{#each webring as member, index}
 			{#if member.url === "https://www.raygoo.tech/"}
-				<a class="webring-anchor" href={webring[index-1].url ? webring[index-1].url : "https://webring.hackclub.com/"} id="previousBtn" title="Previous">‹</a>
-				<a class="webring-logo" href="https://webring.hackclub.com/"
-					 title="Hack Club Webring"></a>
-				<a class="webring-anchor" href={webring[index+1].url ? webring[index+1].url : "https://webring.hackclub.com/"} id="nextBtn" title="Next">›</a>
+				myIndex = index;
 			{/if}
 		{/each}
+		{@debug myIndex}
+		<a class="webring-anchor" href={myIndex ? webring[myIndex-1] : "https://webring.hackclub.com/"} title="Previous">‹</a>
+		<a class="webring-logo" href="https://webring.hackclub.com/"
+			 title="Hack Club Webring"></a>
+		<a class="webring-anchor"  href={myIndex ? webring[myIndex+1] : "https://webring.hackclub.com/"} title="Next">›</a>
 		{/await}
 </div>
