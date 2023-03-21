d18ebada605c7e10e127932b5992fc1cfd08a20e
Raymond <raymond.goo124@gmail.com>
2023-03-21 14:25:57 +0800

    /routes/birthday: Add birthday page

---
+++ src/routes/birthday/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { DateTime, Interval } from "luxon";
+
+	let birthday = DateTime.local(2023, 3, 24, {zone: "Asia/Singapore"});
+	let timeDiff = Interval.fromDateTimes(DateTime.now(), birthday);
+	setInterval(() => {
+		timeDiff = Interval.fromDateTimes(DateTime.now(), birthday);
+	}, 50);
+</script>
+
+<div>
+	{#if timeDiff.toDuration(['days', 'hours', 'minutes', 'seconds']).toObject().seconds === undefined}
+	    <h1 class="font-bold inter-font text-5xl">
+				Happy Birthday!
+			</h1>
+	{:else}
+	<h1 class="font-bold inter-font text-5xl pb-4">My birthday is in...</h1>
+	<p class="text-2xl">
+		<span>{timeDiff.toDuration(['days', 'hours', 'minutes', 'seconds']).toObject().days} days</span>
+		<span>{timeDiff.toDuration(['days', 'hours', 'minutes', 'seconds']).toObject().hours} hours</span>
+		<span>{timeDiff.toDuration(['days', 'hours', 'minutes', 'seconds']).toObject().minutes} minutes</span>
+		<span>{timeDiff.toDuration(['days', 'hours', 'minutes', 'seconds']).toObject().seconds.toFixed(2)} seconds</span>
+	</p>
+	{/if}
+</div>
