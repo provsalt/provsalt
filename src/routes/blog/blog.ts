eb54ca895ab10530cfd349e1a164fea30756dced
Raymond <raymond.goo124@gmail.com>
2023-11-08 00:44:44 +0800

    routes/blogs/blogs.ts: New blog

--- src/routes/blog/blog.ts
+++ src/routes/blog/blog.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { html as Experience } from "$lib/blogs/my-experience-with-sveltekit/blog.md";
 import { html as FOSSASIA23 } from "$lib/blogs/fossasia_23/blog.md";
+import { html as Japanese30Days } from "$lib/blogs/30_days_of_japanese/blog.md";
 
 interface Blog {
 	title: string;
@@ -25,5 +26,12 @@ export const Blogs: Blog[] = [
 		date: "2023-04-24",
 		slug: "taking-part-in-fossasia-2023",
 		content: FOSSASIA23
+	},
+	{
+		title: "三十日時に日本語を勉強した",
+		description: "一ヶ月前に日本語を初めてを勉強した、すごいね！",
+		date: "2023-11-06",
+		slug: "30-days-of-japanese",
+		content: Japanese30Days
 	}
 ];
