de04d64d13be07942a595706fbad2699448f733c
Raymond Goo <raymond.goo124@gmail.com>
2026-02-23 04:51:18 +0800

    feat: add sitemap and robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
 import tailwindcss from '@tailwindcss/vite';
 import reactI18next from 'astro-react-i18next';
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://raymond.moe",
   integrations: [react(), partytown(), reactI18next({
     defaultLocale: "en",
     locales: ["en", "ja"],
-  })],
+  }), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
